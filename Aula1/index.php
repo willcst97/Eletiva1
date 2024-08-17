c7af8f48a6e9d069fa5b25947cccea3880902e7d
willcst97 <w.williamcosta@gmail.com>
2024-08-17 11:08:11 -0300

    Mudanças no exerc

--- Aula1/index.php
+++ Aula1/index.php
@@ -7,7 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>hoje é dia <?php echo date("d/m/Y"); ?></h1>
+    <h1>hoje é dia <?php echo date("d/m/y"); ?></h1>
+    <form action="Aula1/resposta.php" method="POST">
+        <input type="text" name="valor"/>
+        <button type="submit">Enviar</button>
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html
