c02bc1bc72d5174bfb446aa290c0292960472552
willcst97 <125269341+willcst97@users.noreply.github.com>
2024-08-20 19:40:52 -0300

    atualizações do exemplo

--- Aula1/resposta.php
+++ Aula1/resposta.php
@@ -3,12 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resposta</title>
 </head>
 <body>
     <?php 
-        $valor = $_POST["valor"];
+        //Este é um comentário de uma linha
+        #Este é um comentário de uma linha
+        /*
+        Este
+        é um comentário em bloco
+        */
+        $valor = (int) $_POST["valor"];
+        $valor = (float) $_POST["valor"];
+        $valor = (string) $_POST["valor"];
+        //$valor = (bool) $_POST["valor"];
         echo "Valor informado: $valor";
+        echo 'Valor informado: '.$valor;
     ?>
 </body>
 </html>
