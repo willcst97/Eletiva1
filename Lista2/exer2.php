a68954de3c1667e664a6cc599f452a744b759a52
willcst97 <w.williamcosta@gmail.com>
2024-08-24 10:33:37 -0300

    Término do exercício 2

--- Lista2/exer2.php
+++ Lista2/exer2.php
@@ -11,11 +11,11 @@
     <form action="exer2resp.php" method="post">
         <div class="row">
             <div class="col">
-                <label for="valor1" class="form-label">Informe o valor a subtrair:</label>
+                <label for="valor1" class="form-label">Informe o valor a ser subtraído:</label>
                 <input type="number" class="form-control" name="valor1" id="valor1">
             </div>
             <div class="col">
-                <label for="valor2" class="form-label">Informe o valor a ser subtraído:</label>
+                <label for="valor2" class="form-label">Informe o valor a subtrair:</label>
                 <input type="number" class="form-control" name="valor2" id="valor2">
             </div>
         </div>
