0207cedcbd2c8892a5d4d652c75e5d8e1a268228
willcst97 <w.williamcosta@gmail.com>
2024-08-24 10:39:08 -0300

    Início do exercício 3

--- Lista2/exer3.php
+++ Lista2/exer3.php
@@ -3,25 +3,15 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 2</title>
+    <title>Exercício 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container">
-    <h1>Exercício 2: subtração</h1>
+    <h1>Exercícicio 3: multiplicação</h1>
     <form action="exer2resp.php" method="post">
         <div class="row">
             <div class="col">
-                <label for="valor1" class="form-label">Informe o valor a ser subtraído:</label>
-                <input type="number" class="form-control" name="valor1" id="valor1">
-            </div>
-            <div class="col">
-                <label for="valor2" class="form-label">Informe o valor a subtrair:</label>
-                <input type="number" class="form-control" name="valor2" id="valor2">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <button type="submit" class="btn mt-3 btn-primary">Enviar</button>
+                
             </div>
         </div>
     </form>
