2a425578f672649c4d57739e409af171833510ee
willcst97 <w.williamcosta@gmail.com>
2024-08-24 10:42:03 -0300

    Atualização do exercício 3 com o primeiro input

--- Lista2/exer3.php
+++ Lista2/exer3.php
@@ -7,11 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container">
-    <h1>Exercícicio 3: multiplicação</h1>
+    <h1>Exercício 3: multiplicação</h1>
     <form action="exer2resp.php" method="post">
         <div class="row">
             <div class="col">
-                
+                <label for="valor1" class="form-label">Informe o primeiro valor:</label>
+                <input type="number" class="form-control" name="valor1" id="valor1">
             </div>
         </div>
     </form>
