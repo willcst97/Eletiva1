a18452351787228972c68271e035759a07eba9fe
William Costa <w.williamcosta@gmail.com>
2024-09-07 19:35:57 -0300

    Início ex7 lista3

---
+++ Lista3/exer07resp.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Resposta do exercício 2</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="container">
+  <h1>Resposta do exercício 6</h1>
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    try {
+      $valor = (int) $_POST['valor'] ?? 0;
+      $i = 1;
+      while ($i <= 5) {
+        echo "$i \n";
+        $i++;
+      }
+    } catch (Exception $e) {
+      echo "Erro! " . $e->getMessage();
+    }
+  }
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
