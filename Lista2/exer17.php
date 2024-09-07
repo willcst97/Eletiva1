2ec6aba75343ce94984e3b0df6caa775aae2a766
William Costa <w.williamcosta@gmail.com>
2024-09-07 17:08:07 -0300

    Início ex17 lista2

--- Lista2/exer17.php
+++ Lista2/exer17.php
@@ -4,20 +4,25 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exercício 15</title>
+  <title>Exercício 17</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body class="container">
-  <h1>Exercício 16: calcular desconto</h1>
-  <form action="exer16resp.php" method="post">
+  <h1>Exercício 17: calcular juros simples</h1>
+  <form action="exer17resp.php" method="post">
     <div class="row">
       <div class="col">
-        <label for="valor1" class="form-label">Informe o preço:</label>
+        <label for="valor1" class="form-label">Informe o capital:</label>
         <input type="text" class="form-control" name="valor1" id="valor1">
-      </div><div class="col">
-        <label for="valor2" class="form-label">Informe o percentual de desconto:</label>
+      </div>
+      <div class="col">
+        <label for="valor2" class="form-label">Informe a taxa de juros:</label>
         <input type="text" class="form-control" name="valor2" id="valor2">
+      </div>
+      <div class="col">
+        <label for="valor3" class="form-label">Informe o período:</label>
+        <input type="text" class="form-control" name="valor3" id="valor3">
       </div>
       <div class="row">
         <div class="col">
