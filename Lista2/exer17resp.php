2e0270ba7b56767a5ba4a2b85aab0df46a2099ac
William Costa <w.williamcosta@gmail.com>
2024-09-07 17:05:28 -0300

    Correção ex16 lista2

--- Lista2/exer17resp.php
+++ Lista2/exer17resp.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Resposta do exercício 16</title>
+  <title>Resposta do exercício 15</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body class="container">
-  <h1>Resposta do exercício 16</h1>
+  <h1>Resposta do exercício 15</h1>
   <?php
   if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     try {
