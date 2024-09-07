66f178be9afdb570bc3e335b094810421133baf4
William Costa <w.williamcosta@gmail.com>
2024-09-07 19:28:46 -0300

    Finalização ex5 início ex6 lista3

---
+++ Lista3/exer06.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercício 6</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="container">
+  <h1>Exercício 6: completando os números</h1>
+  <p>Informe um valor e terá os números de 1 até o informado como retorno.</p>
+  <form action="exer06resp.php" method="post">
+    <div class="row mt-2">
+      <div class="col-6">
+        <label for="valor" class="form-label">Informe o valor:</label>
+        <input type="text" class="form-control" name="valor" id="valor">
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </div>
+    </div>
+  </form>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
