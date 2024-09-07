a423503e0209ae8bc2ecc71ef71310a3c501191a
William Costa <w.williamcosta@gmail.com>
2024-09-07 15:16:20 -0300

    Início do ex5 Lista 2

---
+++ Lista2/exer5.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 5</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="container">
+    <h1>Exercício 5: calcular média</h1>
+    <form action="exer4resp.php" method="post">
+        <div class="row">
+            <div class="col">
+                <label for="valor1" class="form-label">Informe a primeira nota:</label>
+                <input type="number" class="form-control" name="valor1" id="valor1">
+            </div>
+            <div class="col">
+                <label for="valor2" class="form-label">Informe a segunda nota:</label>
+                <input type="number" class="form-control" name="valor2" id="valor2">
+            </div>
+            <div class="col">
+                <label for="valor2" class="form-label">Informe a terceira nota:</label>
+                <input type="number" class="form-control" name="valor2" id="valor2">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn mt-3 btn-primary">Calcular</button>
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
