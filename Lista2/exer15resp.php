5db03626459b734c82492426637c096fdb5c2eaf
William Costa <w.williamcosta@gmail.com>
2024-09-07 16:49:29 -0300

    Início ex15 lista2

---
+++ Lista2/exer15resp.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Resposta do exercício 14</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="container">
+  <h1>Resposta do exercício 14</h1>
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    try {
+      $valor1 = (float) $_POST['valor1'] ?? 0;
+      $resultado = number_format($valor1 * 0.621371, 2, ',', '.');
+      echo "<h3>$valor1 km = $resultado milhas</h3>";
+    } catch (Exception $e) {
+      echo "Erro! " . $e->getMessage();
+    }
+  }
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
