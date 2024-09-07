247437816ccddd1c7c4f3692f08480dc6d1d24e4
William Costa <w.williamcosta@gmail.com>
2024-09-07 15:57:09 -0300

    Finalização ex10 lista2

--- Lista2/exer10.php
+++ Lista2/exer10.php
@@ -4,15 +4,15 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exercício 8</title>
+  <title>Exercício 10</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body class="container">
-  <h1>Exercício 8: área do retângulo</h1>
-  <form action="exer8resp.php" method="post">
+  <h1>Exercício 10: perímetro do retângulo</h1>
+  <form action="exer9resp.php" method="post">
     <div class="row">
-      <div class="col">
+    <div class="col">
         <label for="valor1" class="form-label">Informe a altura do retângulo:</label>
         <input type="number" class="form-control" name="valor1" id="valor1">
       </div>
