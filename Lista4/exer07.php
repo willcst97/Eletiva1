d0e45accbecd5ad09e0c25481d6309adb6e60f7a
William Costa <w.williamcosta@gmail.com>
2024-09-07 23:33:03 -0300

    Início ex7 lista4

---
+++ Lista4/exer07.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercício 7</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="container">
+  <h1>Exercício 7: comparar datas</h1>
+  <p>Informe duas data no formato dd/mm/yyyy e terá de retorno a diferença entre as duas datas.</p>
+  <form action="exer06resp.php" method="post">
+    <div class="row mt-2">
+      <div class="col">
+        <label for="valor1" class="form-label">Informe a data 1:</label>
+        <input type="text" class="form-control" name="valor1" id="valor1">
+      </div>
+      <div class="col">
+        <label for="valor" class="form-label">Informe a data 2:</label>
+        <input type="text" class="form-control" name="valor" id="valor">
+      </div>
+      <div class="row mt-4">
+        <div class="col">
+          <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
+      </div>
+  </form>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
