72035efcba4d83a23c1b71b9996a8d2e3b794be4
willcst97 <125269341+willcst97@users.noreply.github.com>
2024-09-10 19:56:01 -0300

    Correção ex5 Lista4

--- Lista4/exer05.php
+++ Lista4/exer05.php
@@ -8,22 +8,24 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
-  <h1>Exercício 5: raiz quadrada</h1>
-  <p>Informe um valor e terá de retorno a raiz quadrada deste valor.</p>
-  <form action="exer05resp.php" method="post">
-    <div class="row mt-2">
-      <div class="col-6">
-        <label for="valor1" class="form-label">Informe o valor:</label>
-        <input type="text" class="form-control" name="valor1" id="valor1">
-      </div>
-    <div class="row mt-4">
-      <div class="col">
-        <button type="submit" class="btn btn-primary">Enviar</button>
-      </div>
-    </div>
-  </form>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<body>
+  <main class="container">
+    <h1>Exercício 5: raiz quadrada</h1>
+    <p>Informe um valor e terá de retorno a raiz quadrada deste valor.</p>
+    <form action="exer05resp.php" method="post">
+      <div class="row mt-2">
+        <div class="col-6">
+          <label for="valor1" class="form-label">Informe o valor:</label>
+          <input type="text" class="form-control" name="valor1" id="valor1">
+        </div>
+        <div class="row mt-4">
+          <div class="col">
+            <button type="submit" class="btn btn-primary">Enviar</button>
+          </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </main>
 </body>
 
 </html>
--- Lista4/exer05resp.php
+++ Lista4/exer05resp.php
@@ -1,3 +1,6 @@
+<?php
+
+declare(strict_types=1); ?>
 <!doctype html>
 <html lang="pt-br">
 
@@ -8,20 +11,25 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
-  <h1>Resposta do exercício 5</h1>
-  <?php
-  if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    try {
-      $valor = (int) $_POST['valor1'];
-      $resposta = sqrt($valor);
-      echo "<h3>Raiz quadrada de $valor = $resposta</h3>";
-    } catch (Exception $e) {
-      echo "Erro! " . $e->getMessage();
+<body>
+  <main class="container">
+    <h1>Resposta do exercício 5</h1>
+    <?php
+    function raiz($num): float
+    {
+      return sqrt($num);
     }
-  }
-  ?>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+      try {
+        $valor = (int) $_POST['valor1'];
+        echo "<h3>Raiz quadrada de $valor = " . raiz($valor) . ".</h3>";
+      } catch (Exception $e) {
+        echo "Erro! " . $e->getMessage();
+      }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </main>
 </body>
 
 </html>
