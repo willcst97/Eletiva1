918d0a456dd45cf4bbe978cae7ea399fe13f50a2
willcst97 <125269341+willcst97@users.noreply.github.com>
2024-09-10 19:52:39 -0300

    Correção ex4 lista4

--- Lista4/exer04.php
+++ Lista4/exer04.php
@@ -8,31 +8,33 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
-  <h1>Exercício 4: verificar data</h1>
-  <p>Informe um dia, um mês e um ano e terá de retorno a verificação se esta data é válida.</p>
-  <form action="exer04resp.php" method="post">
-    <div class="row mt-2">
-      <div class="col">
-        <label for="valor1" class="form-label">Informe o dia:</label>
-        <input type="text" class="form-control" name="valor1" id="valor1">
+<body>
+  <main class="container">
+    <h1>Exercício 4: verificar data</h1>
+    <p>Informe um dia, um mês e um ano e terá de retorno a verificação se esta data é válida.</p>
+    <form action="exer04resp.php" method="post">
+      <div class="row mt-2">
+        <div class="col">
+          <label for="valor1" class="form-label">Informe o dia:</label>
+          <input type="text" class="form-control" name="valor1" id="valor1">
+        </div>
+        <div class="col">
+          <label for="valor2" class="form-label">Informe o mês:</label>
+          <input type="text" class="form-control" name="valor2" id="valor2">
+        </div>
+        <div class="col">
+          <label for="valor3" class="form-label">Informe o ano:</label>
+          <input type="text" class="form-control" name="valor3" id="valor3">
+        </div>
       </div>
-      <div class="col">
-        <label for="valor2" class="form-label">Informe o mês:</label>
-        <input type="text" class="form-control" name="valor2" id="valor2">
+      <div class="row mt-4">
+        <div class="col">
+          <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
       </div>
-      <div class="col">
-        <label for="valor3" class="form-label">Informe o ano:</label>
-        <input type="text" class="form-control" name="valor3" id="valor3">
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        <button type="submit" class="btn btn-primary">Enviar</button>
-      </div>
-    </div>
-  </form>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </main>
 </body>
 
 </html>
--- Lista4/exer04resp.php
+++ Lista4/exer04resp.php
@@ -1,3 +1,6 @@
+<?php
+
+declare(strict_types=1); ?>
 <!doctype html>
 <html lang="pt-br">
 
@@ -8,25 +11,31 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
-  <h1>Resposta do exercício 4</h1>
-  <?php
-  if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    try {
-      $dia = (int) $_POST['valor1'];
-      $mes = (int) $_POST['valor2'];
-      $ano = (int) $_POST['valor3'];
-      if (checkdate($mes, $dia, $ano)) {
-        echo "<h3>$dia/$mes/$ano é uma data válida.</h3>";
+<body>
+  <main class="container">
+    <h1>Resposta do exercício 4</h1>
+    <?php
+    function verificarData($d, $m, $y): void
+    {
+      if (checkdate($m, $d, $y) == true) {
+        echo "<h3>$d/$m/$y é uma data válida.</h3>";
       } else {
-        echo "<h3>$dia/$mes/$ano não é uma data válida.</h3>";
+        echo "<h3>$d/$m/$y não é uma data válida.</h3>";
       }
-    } catch (Exception $e) {
-      echo "Erro! " . $e->getMessage();
     }
-  }
-  ?>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+      try {
+        $dia = (int) $_POST['valor1'];
+        $mes = (int) $_POST['valor2'];
+        $ano = (int) $_POST['valor3'];
+        verificarData($dia, $mes, $ano);
+      } catch (Exception $e) {
+        echo "Erro! " . $e->getMessage();
+      }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </main>
 </body>
 
 </html>
