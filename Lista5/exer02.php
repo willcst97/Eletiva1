12c72d81e5e361cd67ea9f83a07fdeb00877a348
willcst97 <125269341+willcst97@users.noreply.github.com>
2024-09-24 19:29:48 -0300

    mudança gráfica do ex02 lista5

--- Lista5/exer02.php
+++ Lista5/exer02.php
@@ -30,7 +30,7 @@
                 </div>
             </div>
         <?php endfor; ?>
-        <div class="row mt-4 mb-4">
+        <div class="row mb-4">
             <div class="col">
                 <button type="submit" class="btn btn-primary">Enviar</button>
             </div>
