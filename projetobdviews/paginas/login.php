e9524c2bb72954f36f577d8fb30a6f04c9c96240
willcst97 <w.williamcosta@gmail.com>
2024-09-28 10:23:36 -0300

    Atualizações nos botões e posição da div

--- projetobdviews/paginas/login.php
+++ projetobdviews/paginas/login.php
@@ -1,9 +1,9 @@
 <?php require_once 'cabecalho.php'; ?>
 
 <div class="container mt-5">
-    <h2>Login</h2>
     <form method="post" action="dashboard.php">
-        <div class="row shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="row shadow p-3 mb-5 bg-body-tertiary rounded position-relative">
+            <h2>Login</h2>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" name="email" class="form-control" id="email" required>
@@ -12,9 +12,9 @@
                 <label for="senha" class="form-label">Senha</label>
                 <input type="password" name="senha" class="form-control" id="senha" required>
             </div>
-            <div>
+            <div class="mb-3 d-grid gap-2 col-6 mx-auto">
                 <button type="submit" class="btn btn-primary">Entrar</button>
-                <button type="button" class="btn btn-outline-secondary">Criar conta</button>
+                <a href='novo_usuario.php' class="btn btn-outline-secondary">Criar conta</a>
             </div>
         </div>
     </form>
