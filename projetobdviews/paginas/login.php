192b5ff1003ec933274da5e35f186a877d959b02
willcst97 <w.williamcosta@gmail.com>
2024-09-28 10:03:28 -0300

    Mudanças no botão e div

--- projetobdviews/paginas/login.php
+++ projetobdviews/paginas/login.php
@@ -3,15 +3,20 @@
 <div class="container mt-5">
     <h2>Login</h2>
     <form method="post" action="dashboard.php">
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" name="email" class="form-control" id="email" required>
+        <div class="row shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="senha" class="form-label">Senha</label>
+                <input type="password" name="senha" class="form-control" id="senha" required>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary">Entrar</button>
+                <button type="button" class="btn btn-outline-secondary">Criar conta</button>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="senha" class="form-label">Senha</label>
-            <input type="password" name="senha" class="form-control" id="senha" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Entrar</button>
     </form>
 </div>
 
