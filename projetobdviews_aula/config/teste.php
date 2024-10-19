68fcc702a9832c2160a94f0dbb4d4e1e6734fbdd
willcst97 <w.williamcosta@gmail.com>
2024-10-19 10:15:23 -0300

    Aula de validação de e-mail e senha

---
+++ projetobdviews_aula/config/teste.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+require_once('.../config/bancodedados.php');
+
+function login(string $email, string $senha)
+{
+    global $pdo;
+
+    //Inserção do usuario adm
+    $stament =
+        $pdo->query("SELECT * FROM usuario Where email = 'adm@adm.com'");
+    $usuario = $stament->fetchAll(PDO::FETCH_ASSOC);
+    //verificando se o usuário não existe, caso positivo, vamos criar
+    if (!$usuario) {
+        $senha = password_hash('adm', PASSWORD_BCRYPT);
+        $stament = $pdo->prepare('INSERT INTO usuario (nome,email,senha,nivel) VALUES (?, ?, ?, ?)');
+        $stament->execute(['Administrador', 'adm@adm.com', $senha, 'adm']);
+    }
+
+    //verificar email e senha do usuário
+    $stament =
+        $pdo->prepare("SELECT * FROM usuario WHERE email = ? AND senha = ?"); //validar os valores com EXPRESSÔES REGULARES - validar se é um email
+    $stament->execute([$email, $senha]);
+    $usuario = $stament->fetch(PDO::FETCH_ASSOC);
+    return $usuario ? $usuario : null;
+}
