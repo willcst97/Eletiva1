1ebb71b0dba69c5ff87984d5965a4a8aa1560fef
willcst97 <125269341+willcst97@users.noreply.github.com>
2024-11-05 19:58:28 -0300

    atualização da aula

--- projetobdviews_aula/paginas/editar_produto.php
+++ projetobdviews_aula/paginas/editar_produto.php
@@ -16,12 +16,38 @@
     }
 
     $categorias = buscarCategorias();
+
+    $erro = "";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        try {
+            $nome = $_POST['nome'];
+            $descricao = $_POST['descricao'];
+            $preco = $_POST['preco'];
+            $estoque_minimo = intval($_POST['estoque_minimo']);
+            $categoria_id = intval($_POST['categoria_id']);
+            $id = intval($_POST['id']);
+            if (empty($nome)){
+                $erro = 'Preencha os campos obrigatórios!'; //fazer assim com todos os dados obrigatórios
+            } else {
+                if (alterarProduto($id, $nome, $descricao, $preco, $estoque_minimo, $categoria_id)){
+                    header('Location: produtos.php');
+                } else {
+                    $erro = "Erro ao alterar produto!";
+                    exit();
+                }
+            }
+        } catch (Exception $e) {
+            $erro = "Erro: ".$e->getMessage();
+        }
+    }
 ?>
 
 <div class="container mt-5">
     <h2>Editar Produto</h2>
 
     <form method="post">
+        <input type="hidden" name="id" value="<?= $id ?>" />
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" name="nome" id="nome" value="<?= $produto['nome'] ?>" class="form-control" value="" required>
@@ -29,7 +55,7 @@
         <div class="mb-3">
             <label for="descricao" class="form-label">Descrição</label>
             <textarea name="descricao" id="descricao" class="form-control" required>
-                value="<?= $produto['descricao'] ?>"
+                <?= $produto['descricao'] ?>
             </textarea>
         </div>
         <div class="mb-3">
