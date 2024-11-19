45f5bf485a63b96d079f598b21ab3f382a436b56
willcst97 <125269341+willcst97@users.noreply.github.com>
2024-11-19 19:59:03 -0300

    Atualizações estéticas e no banco de dados

--- projetobdviews/config/bancodedados.php
+++ projetobdviews/config/bancodedados.php
@@ -1,19 +1,29 @@
-<?php
+<div class="container text-center">
+    <div class="col">
+        <div class="row mt-3">
+            <?php
 
-    $host = "localhost";
-    $db = "banco_php";
-    $usuario = "root";
-    $senha = "";
-    $port = "3306";
+            $host = "localhost";
+            $db = "banco_php";
+            $usuario = "root";
+            $senha = "";
+            $port = "3306";
 
-    try{
-        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", 
-                        $usuario, $senha);
-        if ($pdo){
-            echo "Conexão realizada com sucesso!";
-        } else {
-            echo "Erro ao conectar o banco de dados!";
-        }
-    } catch (Exception $e){
-        echo "Erro: ".$e->getMessage();
-    }
+            try {
+                $pdo = new PDO(
+                    "mysql:host=$host;port=$port;dbname=$db",
+                    $usuario,
+                    $senha
+                );
+                if ($pdo) {
+                    echo "Conexão com o banco de dados realizada com sucesso!";
+                } else {
+                    echo "Erro ao conectar o banco de dados!";
+                }
+            } catch (Exception $e) {
+                echo "Erro: " . $e->getMessage();
+            }
+            ?>
+        </div>
+    </div>
+</div>
