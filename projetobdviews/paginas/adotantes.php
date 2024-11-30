35c3da0244d54e1111a2cfd966fda5db88d27189
William Costa <w.williamcosta@gmail.com>
2024-11-30 02:11:00 -0300

    Início CRUD Adotantes

--- projetobdviews/paginas/adotantes.php
+++ projetobdviews/paginas/adotantes.php
@@ -1,38 +1,37 @@
 <?php 
     require_once 'cabecalho.php'; 
     require_once 'navbar.php';  
-    require_once '../funcoes/animais.php';
+    require_once '../funcoes/adotantes.php';
     
-    $produtos = buscarAnimais();
+    $adotantes = buscarAdotantes();
 ?>
 
 <div class="container mt-5">
-    <h2>Gerenciamento de Animais</h2>
-    <a href="novo_produto.php" class="btn btn-success mb-3">Cadastrar Animal</a>
+    <h2>Gerenciamento de Adotantes</h2>
+    <a href="novo_adotante.php" class="btn btn-success mb-3">Cadastrar Adotante</a>
     <table class="table table-hover table-striped">
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
-                <th>Descrição</th>
+                <th>Endereço</th>
                 <th>Idade</th>
-                <th>Tipo</th>
-                <th>ONG Responsável</th>
+                <th>Telefone</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody>
             
-            <?php foreach($produtos as $p) : ?>
+            <?php foreach($adotantes as $a) : ?>
             <tr>
-                <td><?= $p['id'] ?></td>
-                <td><?= $p['nome'] ?></td>
-                <td><?= $p['descricao'] ?></td>
-                <td><?= $p['idade'] ?></td>
-                <td><?= $p['tipo'] ?></td>
-                <td><?= $p['ong'] ?></td>
+                <td><?= $a['id'] ?></td>
+                <td><?= $a['nome'] ?></td>
+                <td><?= $a['endereco'] ?></td>
+                <td><?= $a['idade'] ?></td>
+                <td><?= $a['fone'] ?></td>
                 <td>
-                    <a href="editar_produto.php?id=<?= $p['id'] ?>" class="btn btn-warning">Editar</a>
-                    <a href="excluir_produto.php?id=<?= $p['id'] ?>" class="btn btn-danger">Excluir</a>
+                    <a href="editar_adotante.php?id=<?= $a['id'] ?>" class="btn btn-warning">Editar</a>
+                    <a href="excluir_adotante.php?id=<?= $a['id'] ?>" class="btn btn-danger">Excluir</a>
                 </td>
             </tr>
             <?php endforeach; ?>
